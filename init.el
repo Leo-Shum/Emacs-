0a2afdad1491e5427316bdf64c4449e6c04f7906
Leo <cenliwen03@gmail.com>
2021-12-14 03:31:14 +0800

    update

--- init.el
+++ init.el
@@ -18,86 +18,48 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; ----------------------------------------------------------------
+; ------------------------- my config -------------------------
 (setq inhibit-compacting-font-caches t) ;; Chinese font bu hui ka
 
 
-;; 显示行号
+; 显示行号
 (global-linum-mode 1)
 (setq linum-format "%d ")
 
+; C/C++ 缩进4
+; 设锤子 用 cc-mode
 
-;; C/C++ 缩进4
-;; 设锤子 用 cc-mode
+; backtab
+; 选中*一整行* C-x TAB
+; https://stackoverflow.com/questions/2249955/emacs-shift-tab-to-left-shift-the-block/2252922
 
+; 字体
+(set-frame-font "JetBrains Mono")
 
-;; backtab
-;; https://stackoverflow.com/questions/23692879/emacs24-backtab-is-undefined-how-to-define-this-shortcut-key
-(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
-(defun un-indent-by-removing-4-spaces ()
-  "remove 4 spaces from beginning of of line"
-  (interactive)
-  (save-excursion
-    (save-match-data
-      (beginning-of-line)
-      ;; get rid of tabs at beginning of line
-      (when (looking-at "^\\s-+")
-        (untabify (match-beginning 0) (match-end 0)))
-      (when (looking-at "^    ")
-        (replace-match "")))))
+; 自动补全
+; https://zhuanlan.zhihu.com/p/19935656
 
-;; 字体
-;; (set-default-font "Consolas")
-(set-default-font "JetBrains Mono")
-
-
-;; 中文乱码，什么乱码啊，都 utf-8
-;; https://emacs-china.org/t/revert-buffer-with-coding-system-utf-8/9415
-;; https://emacs-china.org/t/coding-system-utf-8-org/9429
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;Coding system
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-;; backwards compatibility as default-buffer-file-coding-system
-;; is deprecated in 23.2.
-(if (boundp 'buffer-file-coding-system)
-    (setq-default buffer-file-coding-system 'utf-8)
-  (setq default-buffer-file-coding-system 'utf-8))
-
-;; Treat clipboard input as UTF-8 string first; compound text next, etc.
-(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
-
-
-;; 自动补全
-;; https://zhuanlan.zhihu.com/p/19935656
-;; 配置melpa Packages源
-
-;; 关闭 welcome 界面
-;; https://blog.csdn.net/grey_csdn/article/details/79008464
+; 关闭 welcome 界面 https://blog.csdn.net/grey_csdn/article/details/79008464
 (setq inhibit-splash-screen t)
 
+; Tab显示长度4 https://oi-wiki.org/lang/editor/emacs/
+; (setq-default default-tab-width 4)
+; (setq-default tab-width 4)
 
-;; ----------------------------------------------------------------
-
-;; 参考来自 https://oi-wiki.org/lang/editor/emacs/
-;;Tab显示长度4
-(setq-default default-tab-width 4)
-(setq-default tab-width 4)
-
-;;高亮
+; 高亮
 (global-hl-line-mode 1);;高亮当前行
 (show-paren-mode t);;高亮匹配括号
-;;(global-font-lock-mode t);;语法高亮
+; (global-font-lock-mode t);;语法高亮
 
-;;设置光标形状为竖线（默认为方块）
+; 设置光标形状为竖线（默认为方块）
 (setq-default cursor-type 'bar)
 
-;;设置默认编码环境
+; 设置默认编码环境
 (set-language-environment "UTF-8")
-;;(set-default-coding-systems 'utf-8)
+; (set-default-coding-systems 'utf-8)
 
-;; 关闭 autosave
+; 关闭 autosave
 (setq auto-save-default nil)
+
+; 取消备份
+(setq make-backup-files nil)
